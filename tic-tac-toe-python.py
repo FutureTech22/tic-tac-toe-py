cb4256412eb0d3d9871a3a010e8d5361b7d188d7
Brett Williams <b_m_williams96@yahoo.com>
2017-12-17 13:36:45 -0500

    working but need AI

--- tic-tac-toe-python.py
+++ tic-tac-toe-python.py
@@ -16,6 +16,17 @@
 #check for second player win
 #check for a full board
 
+#stage4:
+#combine stages 2 & 3 into 1 function
+#def is winner(board, player):
+#create a function to check if the board is full
+#def is_board_full(board)
+
+#stage5
+#creating our game AI
+#def get_computer_move(board,player):
+#return random number
+#make sure random number is an empty space
 
 #import
 import os
@@ -29,8 +40,7 @@ board = [""," "," "," "," "," "," "," "," "," ",]
 def print_header():
 	print "TIC TAC TOE 3 IN A ROW"
 
-
-	#define the print board function
+#define the print board function
 def print_board():
 	print "   |   | "
 	print " "+board[1]+" | "+board[2]+" | "+board[3]+" "
@@ -43,6 +53,27 @@ def print_board():
 	print "   |   |   "
 	print " "+board[7]+" | "+board[8]+" | "+board[9]+" "
 	print "   |   |   "
+
+#check for winner
+def is_winner(board, player):
+	if (board[1] == player and board[2] == player and board[3] == player) or\
+	 	(board[4] == player and board[5] == player and board[6] == player) or\
+		(board[7] == player and board[8] == player and board[9] == player) or\
+		(board[1] == player and board[4] == player and board[7] == player) or\
+		(board[2] == player and board[5] == player and board[8] == player) or\
+		(board[3] == player and board[6] == player and board[9] == player) or\
+		(board[1] == player and board[5] == player and board[9] == player) or\
+		(board[3] == player and board[5] == player and board[7] == player):
+		return True
+	else:
+		return False
+
+#check for a tie
+def is_full(board):
+	if " " in board:
+		return False
+	else:
+		return True
 
 while True:
 	os.system("clear")
@@ -61,36 +92,17 @@ while True:
 		time.sleep(1)
 
 	#check for X win
-	if (board[1] == "X" and board[2] == "X" and board[3] == "X") or\
-	 	(board[4] == "X" and board[5] == "X" and board[6] == "X") or\
-		(board[7] == "X" and board[8] == "X" and board[9] == "X") or\
-		(board[1] == "X" and board[4] == "X" and board[7] == "X") or\
-		(board[2] == "X" and board[5] == "X" and board[8] == "X") or\
-		(board[3] == "X" and board[6] == "X" and board[9] == "X") or\
-		(board[1] == "X" and board[5] == "X" and board[9] == "X") or\
-		(board[3] == "X" and board[5] == "X" and board[7] == "X"):
+	if is_winner(board, "X"):
 		os.system("clear")
 		print_header()
 		print_board()
 		print "X Wins! Congratulations"
 		break
 
-	#check for a tie (if board is full)
-	isFull = True
-	if " " in board:
-		isFull = False
-
 	#If the board is full, do something
-	if isFull == True:
+	if is_full(board) == True:
 		print "Tie!"
 		break	
-
-
-
-#	for index in range(1, 10):
-#		if board[index] == " ":
-#			isFull = False
-#			break
 
 	os.system("clear")
 	print_header()
@@ -108,14 +120,7 @@ while True:
 		time.sleep(1)
 
 	#check for O win
-	if (board[1] == "O" and board[2] == "O" and board[3] == "O") or\
-	 	(board[4] == "O" and board[5] == "O" and board[6] == "O") or\
-		(board[7] == "O" and board[8] == "O" and board[9] == "O") or\
-		(board[1] == "O" and board[4] == "O" and board[7] == "O") or\
-		(board[2] == "O" and board[5] == "O" and board[8] == "O") or\
-		(board[3] == "O" and board[6] == "O" and board[9] == "O") or\
-		(board[1] == "O" and board[5] == "O" and board[9] == "O") or\
-		(board[3] == "O" and board[5] == "O" and board[7] == "O"):
+	if is_winner(board, "O"):
 		os.system("clear")
 		print_header()
 		print_board()
