10bdacf2eeb4abf9aa17ce7db392a51c125217c7
Brett Williams <b_m_williams96@yahoo.com>
2017-12-18 15:19:54 -0500

    simple AI

--- tic-tac-toe-python.py
+++ tic-tac-toe-python.py
@@ -75,6 +75,37 @@ def is_full(board):
 	else:
 		return True
 
+def get_computer_move(board,player):
+
+	if board[1] == player and board[4] == player and board[7] == " ":
+		return 7
+	if board[4] == player and board[7] == player and board[1] == " ":
+		return 1
+	if board[1] == player and board[7] == player and board[4] == " ":
+		return 4
+	if board[2] == player and board[5] == player and board[8] == " ":
+		return 8
+	if board[5] == player and board[8] == player and board[2] == " ":
+		return 2
+	if board[2] == player and board[8] == player and board[5] == " ":
+		return 5
+	if board[3] == player and board[6] == player and board[9] == " ":
+		return 9
+	if board[6] == player and board[9] == player and board[3] == " ":
+		return 3
+	if board[3] == player and board[9] == player and board[6] == " ":
+		return 6
+	#if the center square is empty choose that
+	if board[5] == " ":
+		return 5
+	while True:
+		move = random.randint(1,9)
+		#if space is empty go ahead and return, otherwise try again
+		if board[move] == " ":
+			return move
+			break
+
+
 while True:
 	os.system("clear")
 	print_header()
@@ -109,8 +140,7 @@ while True:
 	print_board()
 
 	#get player O input
-	choice = raw_input("Please choose an empty space for O:")
-	choice = int(choice)
+	choice = get_computer_move(board, "O")
 
 	#choice to see if the space is empty first
 	if board[choice] == " ":
